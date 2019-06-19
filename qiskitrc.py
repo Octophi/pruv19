1e76761f62bc0ecd0242374f9bb2a55262fdffe3
octophi <joey.li@duke.edu>
2019-06-19 16:17:32 -0400

    added new matrix work + comments

--- qiskitrc.py
+++
@@ -1,2 +0,0 @@
-from qiskit import IBMQ
-IBMQ.save_account('96c86855a5c381875a4ea3a956a7deafbbbbb82e5912ec73655c7c444d2597a7364eda2fa558e4e37f672640029443216899825f945d5a0684e31b796837ce61', 'https://api.quantum-computing.ibm.com/api/Hubs/ibm-q/Groups/open/Projects/main')
